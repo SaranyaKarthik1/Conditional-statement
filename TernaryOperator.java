91d30d40e4f44e7d21401f403c6cceaed41130c1
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-09-07 17:45:37 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,16 @@
+package Topics2;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b=20;
+System.out.println("The first number is " +a);
+System.out.println("The second number is " +b);
+//using ternary operator.
+int c=(a>b)? a:b;
+
+System.out.println("Using ternary condition : Greater is " +c);
+	}
+
+}
